472a96e1467e357b263e53111c337e212a4795c3
RavemasterZ1 <emile_epa@hotmail.fr>
2022-07-07 11:44:03 -0700

    adding of the 4-put.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int put(const char *str)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
+
+int main ()
+{
+  char *str;
+  put(str);
+  return 0;
+}
