583342f672f7e420577e7454ce0e015b8d469ade
RavemasterZ1 <emile_epa@hotmail.fr>
2022-07-08 14:47:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
+int main(void)
 {
-  write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
-  write(1,"\n",1);
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+  write(1, "\n" ,1);
   return 1;
 }
