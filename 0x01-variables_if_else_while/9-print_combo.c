ec85a439921daf609b65d06c98b0813c4b8d9d8d
RavemasterZ1 <emile_epa@hotmail.fr>
2022-07-10 10:50:13 -0700

    last push of the exercise

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int n;
+
+  for (n = 48; n <= 57; n++)
+    {
+      putchar(n);
+      if (n != 57)
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+  return 0;
+}
