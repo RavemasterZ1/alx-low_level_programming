1b3fabc993a99a2bda53ec70b41bcba369c0ed20
RavemasterZ1 <emile_epa@hotmail.fr>
2022-07-15 18:33:10 -0700

    new start

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+  int n;
+  int n1;
+
+  for (n = 0; n <= 9; n++)
+    {
+      for (n1 = n + 1; n1 <= 9; n1++)
+	{
+	  putchar(n + '0');
+	  putchar(n1 + '0');
+	  if (n != 8 || n1 != 9 )
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+  putchar('\n');
+  return 0;
+}
